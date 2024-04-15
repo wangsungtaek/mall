b12defc184ef9c5c111152c20b3f4d9b9307994d
wangsungtaek <tjdxorv@naver.com>
2024-04-15 23:23:29 +0900

    add REST Controller

---
+++ backend/mallapi/src/main/java/org/next/mallapi/config/CustomServletConfig.java
@@ -0,0 +1,22 @@
+package org.next.mallapi.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.next.mallapi.controller.formatter.LocalDateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Log4j2
+public class CustomServletConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
+        log.info("--------------------");
+        log.info("addFormatters");
+
+        registry.addFormatter(new LocalDateFormatter());
+    }
+}
--- backend/mallapi/src/main/java/org/next/mallapi/controller/TodoController.java
+++ backend/mallapi/src/main/java/org/next/mallapi/controller/TodoController.java
@@ -6,10 +6,9 @@ import org.next.mallapi.dto.PageRequestDTO;
 import org.next.mallapi.dto.PageResponseDTO;
 import org.next.mallapi.dto.TodoDTO;
 import org.next.mallapi.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -29,5 +28,34 @@ public class TodoController {
     public PageResponseDTO<TodoDTO> getTodoList(PageRequestDTO pageRequestDTO) {
 
         return todoService.getList(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO dto) {
+
+        log.info("todo DTO: " + dto);
+
+        Long tno = todoService.register(dto);
+
+        return Map.of("TNO", tno);
+
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable("tno") Long tno, @RequestBody TodoDTO dto) {
+
+        dto.setTno(tno);
+
+        todoService.modity(dto);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable("tno") Long tno) {
+
+        todoService.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
     }
 }
