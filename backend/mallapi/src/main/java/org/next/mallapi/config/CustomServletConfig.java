0fbc7551ad5ee1ed30470f0d3e840509532a2b50
wangsungtaek <tjdxorv@naver.com>
2024-04-16 23:55:56 +0900

    Complete CORS settings configuration

--- backend/mallapi/src/main/java/org/next/mallapi/config/CustomServletConfig.java
+++ backend/mallapi/src/main/java/org/next/mallapi/config/CustomServletConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.next.mallapi.controller.formatter.LocalDateFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,15 @@ public class CustomServletConfig implements WebMvcConfigurer {
         log.info("addFormatters");
 
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedHeaders("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedOrigins("*");
+
     }
 }
